f326e8c639374744e617e5dd32c384a86e47d254
Mohammed Naser <mnaser@vexxhost.com>
2025-10-01 20:28:48 -0400

    clean

--- pkgs/by-name/dpdk/package.nix
+++ pkgs/by-name/dpdk/package.nix
@@ -1,13 +1,13 @@
 {
-  lib,
   pkgs,
   fetchurl,
 }:
 
-pkgs.dpdk.overrideAttrs (oldAttrs: rec {
+pkgs.dpdk.overrideAttrs rec {
   version = "23.11.5";
+
   src = fetchurl {
     url = "https://fast.dpdk.org/rel/dpdk-${version}.tar.xz";
     sha256 = "sha256-ij3/a0O7xYR1IPEFyRaziZhvmTuh6mLnsHpnXFQw4Ww=";
   };
-})
+}
--- pkgs/by-name/openvswitch/package.nix
+++ pkgs/by-name/openvswitch/package.nix
@@ -1,19 +1,20 @@
 {
-  lib,
   pkgs,
   fetchFromGitHub,
 }:
 
 (pkgs.openvswitch.override {
-  withDPDK = true;
   dpdk = pkgs.local.dpdk;
+
+  withDPDK = true;
 }).overrideAttrs
-  (oldAttrs: rec {
+  rec {
     version = "3.3.6";
+
     src = fetchFromGitHub {
       owner = "openvswitch";
       repo = "ovs";
       tag = "v${version}";
       hash = "sha256-78O2ag56PzdfUDzpVzF36ZcaShHJsrVs5TCyWjR8pRU=";
     };
-  })
+  }
